bd39169393a567cf0aed88dd59f1527cff7f0f58
Neo Zidereck de Guzman <zidereckn06@gmail.com>
2025-02-16 12:13:23 -0500

    Challenge exercise: Number24

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -57,9 +57,11 @@ public class MusicOrganizer
      */
     public void listAllFiles()
     {
+        int position = 0;
         for(String filename : files) {
-            System.out.println(filename);
-        }
+            System.out.println(position + " : " + filename);
+            position++;
+            }
     }
     
     /**
